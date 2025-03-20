0372b58212b7151d970e4d858417f84fe1a7c931
Wraymar <tyreesewray14@gmail.com>
2025-03-20 17:04:52 -0400

    statrted the structure for the game page

--- app/src/assets/pages/Game.jsx
+++ app/src/assets/pages/Game.jsx
@@ -1,5 +1,22 @@
 /** @format */
+import Button from "../components/Button.jsx";
 
-const Game = () => {};
+const Game = () => {
+  return (
+    <div className="game-page-body">
+      <div className="gamepage-btn-container">
+        <button text={"Rules"} className={"game-page-controls"} />
+        <button text={"Start Over"} className={"game-page-controls"} />
+        <button text={"Dark Mode"} className={"game-page-controls"} />
+      </div>
+      <div className="page-backdrop">
+        <div className="data-preview">Data Renders Here</div>
+        <div className="grid-container">Cards renders here</div>
+        <div>Time</div>
+      </div>
+      <img />
+    </div>
+  );
+};
 
 export default Game;
