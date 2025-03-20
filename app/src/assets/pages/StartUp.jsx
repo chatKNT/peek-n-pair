b9bc1a70d4aedeb492abe0afd8c31a3908bb0b3a
Wraymar <tyreesewray14@gmail.com>
2025-03-19 23:06:37 -0400

    small tweaks

--- app/src/assets/pages/StartUp.jsx
+++ app/src/assets/pages/StartUp.jsx
@@ -1,18 +1,18 @@
 /** @format */
-import { Link } from 'react-router-dom';
-import Button from '../components/Button.jsx';
+import { Link } from "react-router-dom";
+import Button from "../components/Button.jsx";
 
 const StartUp = () => {
   return (
     <div>
-      <Button text={'Dark'} className={'lightdark-button'} />
+      <Button text={"Dark"} className={"lightdark-button"} />
       <div className="page-backdrop">
         <div className="startup-buttons">
-          <Button text={'Best Times'} />
+          <Button text={"Best Times"} className={"best-times-button"} />
           <Link to="/options">
-            <button className="startup-play-button">Play</button>
+            <button className="startup-play-button">PLAY</button>
           </Link>
-          <Button text={'Rules'} />
+          <Button text={"Rules"} className={"rules-button"} />
         </div>
       </div>
     </div>
