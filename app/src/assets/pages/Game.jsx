257fb2e4ed93dbf290c3939d48d3b2f9ae92cc1c
Wraymar <tyreesewray14@gmail.com>
2025-03-20 21:21:16 -0400

    added small tweaks to the game page

--- app/src/assets/pages/Game.jsx
+++ app/src/assets/pages/Game.jsx
@@ -1,15 +1,18 @@
 /** @format */
+import { Link } from "react-router-dom";
 import Button from "../components/Button.jsx";
 
 const Game = () => {
   return (
     <div className="game-page-body">
-      <div className="gamepage-btn-container">
-        <button text={"Rules"} className={"game-page-controls"} />
-        <button text={"Start Over"} className={"game-page-controls"} />
-        <button text={"Dark Mode"} className={"game-page-controls"} />
+      <div className="gamepage-btn-container fixed-top">
+        <Button text={"Rules"} className={"game-page-controls"} />
+        <Link to="/options">
+          <Button text={"Start Over"} className={"game-page-controls"} />
+        </Link>
+        <Button text={"Dark Mode"} className={"game-page-controls"} />
       </div>
-      <div className="page-backdrop">
+      <div className="game-page-backdrop">
         <div className="data-preview">Data Renders Here</div>
         <div className="grid-container">Cards renders here</div>
         <div>Time</div>
